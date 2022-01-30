161ed50f1a822953c4b3ee8de25ebb057aa343a0
Di Wu <meetwudi@gmail.com>
2022-01-30 11:21:42 -0800

    Also deduplicate by authorized_date

--- functions/src/dedupTransactions.ts
+++ functions/src/dedupTransactions.ts
@@ -4,6 +4,6 @@ import { Transaction } from "plaid";
 export default function dedupTransactions(txns: Transaction[]): Transaction[] {
   return uniqBy(
     txns,
-    (txn) => `${txn.amount}:${txn.authorized_datetime}:${txn.merchant_name}:${txn.name}`,
+    (txn) => `${txn.amount}:${txn.authorized_date}:${txn.authorized_datetime}:${txn.merchant_name}:${txn.name}`,
   );
 }
