971456fa5f15f8dde794f7fde7f6f4a2a35c131b
Di Wu (Workplace, MPK) <wudi@fb.com>
2022-02-01 10:40:23 -0800

    Remove plaid credentials

--- functions/scripts/set-env.example.sh
+++ functions/scripts/set-env.example.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 firebase functions:config:set \
-    plaid.client_id="61f223e81039fb0019529117" \
-    plaid.client_secret="80952790990b0857efe06d7b059e11" \
+    plaid.client_id="<plaid_client_id>" \
+    plaid.client_secret="<plaid_client_secret>" \
     plaid.env="sandbox" \
     plaid.products="auth,transactions" \
     plaid.country_codes="US,CA" \
