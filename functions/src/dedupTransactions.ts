b25928d58c6cb0b565d2ef649f19159a2ab2280f
Di Wu <meetwudi@gmail.com>
2022-01-31 22:35:35 -0800

    Dedup by transaction_id as well

--- functions/src/dedupTransactions.ts
+++ functions/src/dedupTransactions.ts
@@ -1,10 +1,9 @@
+import { uniqBy } from "lodash";
 import { Transaction } from "plaid";
 
 export default function dedupTransactions(txns: Transaction[]): Transaction[] {
-  return txns; // FIXME: Dedup really doesn't work well at the moment
-
-  // return uniqBy(
-  //   txns,
-  //   (txn) => `${txn.account_id}:${txn.amount}:${txn.authorized_date}:${txn.authorized_datetime}:${txn.merchant_name}:${txn.name}`,
-  // );
+  return uniqBy(
+    txns,
+    (txn) => `${txn.account_id}:${txn.amount}:${txn.authorized_date}:${txn.authorized_datetime}:${txn.merchant_name}:${txn.name}:${txn.transaction_id}`,
+  );
 }
