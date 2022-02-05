e296d6bc7360dd9e36f64a6a9c180ec7552b8035
Di Wu <meetwudi@gmail.com>
2022-02-04 21:31:46 -0800

    Use environment variables to configure frontend

--- frontend/src/firebase-init.ts
+++ frontend/src/firebase-init.ts
@@ -7,7 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // TODO: Insert your firebase project web config here
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 initializeApp(firebaseConfig);
